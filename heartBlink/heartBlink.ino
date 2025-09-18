8a2a181a2173da4825eefb300f719331f08eeb5a
Shan Lyons <gprslyons@gmail.com>
2025-09-18 14:40:11 -0700

    Initial heart blink

---
+++ heartBlink/heartBlink.ino
@@ -0,0 +1,28 @@
+#include "Arduino_LED_Matrix.h"
+
+ArduinoLEDMatrix matrix;
+
+void setup() {
+  matrix.begin();
+}
+
+void loop() {
+  // Show a heart
+  static const uint8_t heart[8][12] = {
+    {0,0,1,1,0,0,0,0,1,1,0,0},
+    {0,1,1,1,1,0,0,1,1,1,1,0},
+    {1,1,1,1,1,1,1,1,1,1,1,1},
+    {1,1,1,1,1,1,1,1,1,1,1,1},
+    {0,1,1,1,1,1,1,1,1,1,1,0},
+    {0,0,1,1,1,1,1,1,1,1,0,0},
+    {0,0,0,1,1,1,1,1,1,0,0,0},
+    {0,0,0,0,1,1,1,1,0,0,0,0}
+  };
+
+  matrix.renderBitmap(heart, 8, 12);
+  delay(1000);
+
+  // Clear the matrix
+  matrix.clear();
+  delay(1000);
+}
